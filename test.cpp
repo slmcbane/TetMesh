b5803c4065d50651c862d57a1e9ae8954f27f47b
Sean McBane <sean.mcbane@protonmail.com>
2020-02-09 20:49:59 -0600

    Add a test file w/ routine to dump mesh to stdout

---
+++ test.cpp
@@ -0,0 +1,115 @@
+#include "TetMesh.hpp"
+
+#include <iostream>
+
+template <class CoordT, size_t MaxElementAdjacencies, size_t MaxNodeAdjacencies,
+          size_t NodesPerFace, size_t InternalNodes>
+std::ostream &operator<<(std::ostream &stream,
+                         const msh::TetMesh<CoordT, MaxElementAdjacencies, MaxNodeAdjacencies,
+                                            NodesPerFace, InternalNodes> &mesh)
+{
+    stream << "Mesh Information\n"
+           << "================\n"
+           << "Mesh contains " << mesh.num_vertices() << " vertices:\n";
+
+    for (size_t i = 0; i < mesh.num_vertices(); ++i)
+    {
+        const auto &coord = mesh.coordinate(i);
+        stream << "  (" << coord[0] << ", " << coord[1] << ')';
+        stream << (i < mesh.num_vertices() - 1 ? ",\n" : "\n");
+    }
+
+    stream << "\nMesh contains " << mesh.num_elements() << " elements:\n";
+
+    for (size_t i = 0; i < mesh.num_elements(); ++i)
+    {
+        const auto &el = mesh.element(i);
+        stream << "  Element " << i << ":\n  -----------------\n";
+        stream << "    Control nodes: (" << el.control_nodes[0] << ", "
+               << el.control_nodes[1] << ", " << el.control_nodes[2] << ")\n";
+        stream << "    Adjacent elements: (";
+        for (size_t j = 0; j < el.adjacent_elements.size(); ++j)
+        {
+            stream << el.adjacent_elements[j] << (j + 1 == el.adjacent_elements.size() ? ")\n" : ", ");
+        }
+        stream << "    Element's faces: (" << el.faces[0] << ", "
+               << el.faces[1] << ", " << el.faces[2] << ")\n";
+        if (NodesPerFace != 0)
+        {
+            stream << "    Nodes on faces: ";
+            for (size_t j = 0; j < 3; ++j)
+            {
+                stream << '(';
+                for (size_t k = 0; k < NodesPerFace; ++k)
+                {
+                    stream << el.face_nodes[j][k] << (k == NodesPerFace - 1 ? ")" : ", ");
+                }
+                stream << (j < 2 ? ", " : "\n");
+            }
+        }
+
+        if (InternalNodes != 0)
+        {
+            stream << "    Internal nodes: (";
+            for (size_t j = 0; j < InternalNodes; ++j)
+            {
+                stream << el.internal_nodes[j] << (j + 1 < InternalNodes ? ", " : ")\n");
+            }
+        }
+        stream << '\n';
+    }
+
+    stream << "  Node adjacency information\n"
+           << "  --------------------------\n";
+    
+    for (size_t i = 0; i < mesh.num_nodes(); ++i)
+    {
+        stream << "    Nodes sharing an element with node " << i << ": (";
+        const auto &adj = mesh.adjacent_nodes(i);
+        for (size_t j = 0; j < adj.size(); ++j)
+        {
+            stream << adj[j] << (j == adj.size()-1 ? ")\n" : ", ");
+        }
+    }
+
+    return stream;
+}
+
+const std::array<std::array<double, 2>, 12> nodes = {
+    -1, 1,
+    -1, -1,
+    1, -1,
+    1, 1,
+    0, 1,
+    1, 0,
+    0, -1,
+    -1, 0,
+    -0.5, -0.5,
+    0.25, -0.25,
+    -0.25, 0.25,
+    0.5, 0.5
+};
+
+const std::array<std::array<size_t, 3>, 14> tets = {
+    0, 4, 10,
+    5, 2, 9,
+    9, 2, 6,
+    0, 10, 7,
+    4, 3, 11,
+    3, 5, 11,
+    1, 8, 6,
+    7, 8, 1,
+    4, 11, 10,
+    8, 9, 6,
+    10, 9, 8,
+    11, 5, 9,
+    10, 11, 9,
+    7, 10, 8
+};
+
+int main()
+{
+    const msh::TetMesh<double, 11, 36, 2, 1> mesh(nodes, tets, std::vector<std::vector<int>>());
+    std::cout << mesh;
+    return 0;
+}
